dbbe37a23fcad5487e5b9e0b01ed8942529f03e3
frimus <zna@email.cz>
2026-06-25 14:24:57 +0200

    all one file - so far:  jokes_1.py

---
+++ jokes_1.py
@@ -0,0 +1,39 @@
+from pypdf import PdfReader
+from pathlib import Path
+import re
+
+def extract_text_from_pdf(file_name, get_rid_of_strings):
+
+    """ the name of the function is self-explanatory
+        - file_name is the name of pdf file from which we want to extract the text
+            e.g. "Jokes.pdf" 
+        - get_rid_of_strings is a list of strings we want to remove
+            from extracted text """
+    
+    text = ''
+    pdf = PdfReader(Path(file_name))
+
+    for i in range(pdf.get_num_pages()):
+        text += pdf.get_page(i).extract_text()
+
+    for string in get_rid_of_strings:
+        text = text.replace(string, '')
+
+    return text
+
+def text_to_parts(text):
+    """ remove enumeration and split the text """
+    parts = []
+    # This pattern matches one or more digits and following '.'
+    pattern = r"\d+\."
+    matches = re.split(pattern, text)
+    for item in matches:
+        parts.append(item.strip())
+    return parts
+
+# test
+# remove_list = ["One Liner Jokes"]
+# text = extract_text_from_pdf("Jokes.pdf", remove_list)
+# parts = text_to_parts(text)
+# for joke in parts:
+#     print(joke, '\n')
