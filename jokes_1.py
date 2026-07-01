1e45536363e489fd4663d83ebb6c72cce6c39a0a
Machine <boriscorp0110@gmail.com>
2026-07-01 23:26:32 +0200

    Added the tkinter menu and stuff
    awh man this better work. also add a glowing effect for the future

--- jokes_1.py
+++ jokes_1.py
@@ -6,6 +6,7 @@ import re
 import random
 import math
 import tkinter as tk
+from collections import deque
 
 def extract_text_from_pdf(file_name, get_rid_of_strings):
 
@@ -48,3 +49,22 @@ def get_random_joke(parts):
     return random.choice(parts)
 print(get_random_joke(parts))
 
+#YO MAKE ME A WINDOW
+root = tk.Tk()
+root.title("TheWillOfTheCity")
+#GET INFO RAHHH 
+root.configure(bg='black')
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+#ACTUALLY MAKE THE WINDOW 
+root.geometry(f'{screen_width}x{screen_height}')
+
+label = tk.Label(root, text=get_random_joke(parts), bg= 'black', fg= 'RoyalBlue3', font=("Helvetica", 24), wraplength = screen_width-100)
+label.place(relx=0.5, rely=0.5, anchor='center')
+button = tk.Button(root, text="Get New Joke", command=lambda: label.config(text=get_random_joke(parts)), font=("Helvetica", 16), bg='black', fg='RoyalBlue3', activebackground='black', activeforeground='RoyalBlue3', highlightbackground='black', highlightcolor='black', highlightthickness=0, bd=0)
+button.place(relx=0.5, rely=0.6, anchor='center')
+button.config(borderwidth=0, highlightthickness=0)
+glow = tk.Label(root, text="The will of the city", bg='black', fg='RoyalBlue3', font=("Helvetica", 12))
+glow.place(relx=0.5, rely=0.9, anchor='center')
+#KEEP MY SHI OPEN 
+root.mainloop()
